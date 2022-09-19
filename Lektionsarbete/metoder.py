259616a22ca4f6ed18bc347b31aea1c922888a4f
Max Åstrand <max.astrand@skola.taby.se>
2022-09-19 14:47:48 +0200

    19/9

---
+++ Lektionsarbete/metoder.py
@@ -0,0 +1,22 @@
+
+#-------------------------------------------------------------övning 1----------------------------------------------------------------
+from unittest import skip
+
+
+def myfunction (*input):
+    sum = 0
+    for arg in input:
+        sum += arg
+    print(sum)
+
+myfunction(15,12,15)
+
+#-------------------------------------------------------------övning 2----------------------------------------------------------------
+def food (input, vegan = False):
+    if vegan == True:
+        print("sojamjölk")
+    else:
+        print(input)
+
+food("mjölk")
+food("mjölk", True)
