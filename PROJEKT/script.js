d254ebdac1483860328250e4a0258bf5863f944d
Max Åstrand <max.astrand@skola.taby.se>
2022-10-10 15:21:06 +0200

    superklasser och initiering av klasser

--- PROJEKT/script.js
+++ PROJEKT/script.js
@@ -1,10 +1,60 @@
 class piece {
-    constructor(type, team, xpos, ypos){
-    this.type = type
-    this.team = team
-    this.xpos = xpos
-    this.ypos = ypos
+    constructor(self, height, width){
+        self.height = height
+        self.width = width
     }
-    drawpiece(type, team)
-    
+}
+
+class king extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
+}
+
+class queen extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
+}
+
+class bishop extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
+}
+
+class knight extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
+}
+
+class rook extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
+}
+
+class pawn extends piece {
+    constructor(self, team, height, width){
+        super(height, width)
+        self.team = team
+    }
+
+
 }
