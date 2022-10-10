7fba37c3652f1a5eeb31e2b17e5915a92bd34c39
Max Åstrand <max.astrand@skola.taby.se>
2022-10-10 14:47:34 +0200

    10/10

---
+++ Lektionsarbete/arv och hierarkier.py
@@ -0,0 +1,36 @@
+class Djur:
+    def __init__(self, namn):
+        self.namn = namn
+
+class Fågel(Djur):
+    def __init__(self, namn, vingspann):
+        super().__init__(namn)
+        self.vingspann = vingspann
+
+class Fisk(Djur):
+    def __init__(self, namn, maxdjup):
+        super().__init__(namn)
+        self.maxdjup = maxdjup
+
+class Torsk(Fisk):
+    def __init__(self, namn, maxdjup, hastighet):
+        super().__init__(namn, maxdjup)
+        self.hastighet = hastighet
+
+class Haj(Fisk):
+    def __init__(self, namn, maxdjup, antalTänder):
+        super().__init__(namn, maxdjup)
+        self.antalTänder = antalTänder
+
+
+Torsken = Torsk("Torsken", 300, 30)
+
+Hajen = Haj("Hajen", 280, 40)
+
+def fånga():
+    if Torsken.hastighet >= 30 and Torsken.maxdjup - Hajen.maxdjup > 0:
+        print("bra")
+    else:
+        print("dåligt")
+
+fånga()
