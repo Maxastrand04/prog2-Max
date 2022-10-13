804c43f6c8372024866dc24096b0a35c919f6a1f
Max Åstrand <max.astrand@skola.taby.se>
2022-10-13 13:19:25 +0200

    13/10

--- PROJEKT/script.js
+++ PROJEKT/script.js
@@ -1,60 +1,34 @@
-class piece {
-    constructor(self, height, width){
-        self.height = height
-        self.width = width
-    }
-}
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('send-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
-class king extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
 
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
 
-}
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
 
-class queen extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+})
 
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
 
-}
-
-class bishop extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
-
-
-}
-
-class knight extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
-
-
-}
-
-class rook extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
-
-
-}
-
-class pawn extends piece {
-    constructor(self, team, height, width){
-        super(height, width)
-        self.team = team
-    }
-
-
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
 }
