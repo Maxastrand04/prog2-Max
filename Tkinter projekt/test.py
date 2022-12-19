15ec3c78493071ea388ca499242cd8929cfa8202
Max Åstrand <max.astrand@skola.taby.se>
2022-12-19 13:53:28 +0100

    19/12

--- Tkinter projekt/test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-
-yes = Button(text="du lyckas")
-
-button = Button(text="click me", command=lambda : [yes.pack(), button.place_forget()])
-button.place(relx=0,rely=0.9,anchor="w")
-root.mainloop()
