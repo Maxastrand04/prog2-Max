535a98bd7f47b4adf96f244e92cd86dab26972e9
Max Åstrand <max.astrand@skola.taby.se>
2023-02-23 17:23:43 +0100

    23/2

--- Tkinter projekt/Sänka_skepp_online.py
+++ Tkinter projekt/Sänka_skepp_online.py
@@ -15,6 +15,11 @@ points = [0, 0]     #Poängräknare [0] = spelare 1, [1] = spelare 2, [2] = AI
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5050
+FORMAT = 'utf-8'
+
+HIT = "hit"
+MISS = "miss"
+WIN = "win"
 
 client = None
 
@@ -65,7 +70,7 @@ def connect_to_server(e):
         label.config(text="Felaktig adress försök igen")
 
 
-def hit_ships(row, column, team):   #funktion för när man ska skjuta skepp
+def hit_ships(row, column):   #funktion för när man ska skjuta skepp
 
     #row = vilken rad
     #column = vilken kolumn
@@ -80,20 +85,22 @@ def hit_ships(row, column, team):   #funktion för när man ska skjuta skepp
 
         if gameplay[row][column]["text"] == "":  #Kollar ifall rutan är tom
 
-            square = str(row + column)
-            client.send(square.encode('utf-8'))
+            square = str(row) +"," + str(column)
+            client.send(square.encode(FORMAT))
 
-            if board2_setup[row][column]["text"] != "": #Kollar ifall den rutans koordinater matchar motståndarens utplacerade skepp
-                        
-                gameplay[row][column].config(text= "X", bg="red")    #fyller i rutan som en träff
-
-                points[0] += 1  #Ger poäng till spelare 1
-                        
+            data = client.recv(1024)
+            if not data:
+                client.close()
             else:
-                
-                gameplay[row][column].config(text="O", bg="grey")    #fyller i rutan som en miss
-            
-            turn = players[1]   #Ser till att det blir varannan runda
+                hit_or_miss = data.decode(FORMAT)
+                if hit_or_miss == HIT:
+                    
+                    gameplay[row][column].config(text= "X", bg="red")    #fyller i rutan som en träff
+
+                    points[0] += 1  #Ger poäng till spelare 1
+                else:
+                    
+                    gameplay[row][column].config(text="O", bg="grey")    #fyller i rutan som en miss
 
 
             if check_win(1) is True:    #Kollar om spelare 1 har vunnit och avbryter spelet
@@ -103,18 +110,23 @@ def hit_ships(row, column, team):   #funktion för när man ska skjuta skepp
                 
             else:   #Ifall spelare 1 inte har vunnit så går rundan över till nästa spelare
                 label.config(text=(turn + " tur"))
-        return turn
-    else:
-        while True:
             
+
+            #När din tur är slut så väntar man på att motståndaren ska göra sitt drag
             data = client.recv(1024)
             if not data:
                 client.close()
-                break
             else:
-                data.decode('utf-8').split(',')
-                print(data)
+                row, column = data.decode(FORMAT).split(',')
+                row = int(row)
+                column = int(column)
+                print(row, column)
+                if setup[row][column]["text"] != "":
+                    client.send(HIT.encode(FORMAT))
+                elif setup[row][column]["text"] == "":
+                    client.send(MISS.encode(FORMAT))
                 turn = you
+            return turn
 
     
 def place_ships(row, column, team):
@@ -165,48 +177,6 @@ def check_win(player):  #Kollar ifall en spelare har vunnit genom att man har sk
             return True
 
 
-def new_game():
-
-    for row in range(5):
-
-        for column in range(5):
-
-            setup[row][column] = Button(setup_frame, text="", font=("consolas", 40), width=3, height=1, command= lambda row=row, column=column: place_ships(row, column, 1))
-
-            setup[row][column].grid(row=row,column=column)
-
-
-    for row in range(5):
-
-        for column in range(5):
-
-            gameplay[row][column] = Button(gameplay_frame, text="", font=("consolas", 40), width=3, height=1, command= lambda row=row, column=column: hit_ships(row, column, 1))
-
-            gameplay[row][column].grid(row=row,column=column)
-
-
-
-    #Tar in alla globala variabler för att kunna nollställa dem
-    global player_ships, points
-
-    player_ships = 5
-    
-    points = [0,0]
-
-
-    player1_setup_button.config(bg="#F0F0F0") #Ser till att "CONFIRM" knappen inte är grön
-    player2_setup_button.config(bg="#F0F0F0")
-    
-    for item in window.place_slaves():
-        item.place_forget()
-    
-    label.config(text=("Välkommen till sänka skepp! är det en eller två spelare?"))
-    label.place(relx=0.5, anchor="n")
-
-    host_button.place(relx=0.2,rely=0.9,anchor="w")
-    join_button.place(relx=0.8,rely=0.9,anchor="e")
-
-
 def confirm():  #Funktion för när man ska godkänna positionerna för skeppen
     global player_ships
     if player_ships <= 0:  #Kollar så alla skeppen är placerade
@@ -219,14 +189,14 @@ def confirm():  #Funktion för när man ska godkänna positionerna för skeppen
             player_ships -= 1 #Ser till att det inte går att ta bort skepp när man har tryckt
 
         ready = "READY!"
-        client.send(ready.encode('utf-8'))
+        client.send(ready.encode(FORMAT))
         while True:
             data = client.recv(1024)
             if not data:
                 client.close()
                 break
             else:
-                message = data.decode('utf-8')
+                message = data.decode(FORMAT)
                 if message == ready:
                     break
         gameplay_frame.place(relx= 0, rely= 1, anchor="sw")
@@ -234,6 +204,22 @@ def confirm():  #Funktion för när man ska godkänna positionerna för skeppen
 
 
         label.config(text=(players[0] + " tur"))
+
+        global turn
+        if turn != you:
+            
+            data = client.recv(1024)
+            if not data:
+                client.close()
+            else:
+                row, column = data.decode(FORMAT).split(',')
+                row = int(row)
+                column = int(column)
+                if setup[row][column]["text"] != "":
+                    client.send(HIT.encode(FORMAT))
+                elif setup[row][column]["text"] == "":
+                    client.send(MISS.encode(FORMAT))
+                turn = you
 
 
 def start_multiplayer():
@@ -267,9 +253,6 @@ join_button = Button(window, text="Join", command=lambda : [join_button.place_fo
 join_button.place(relx=0.8,rely=0.9,anchor="e")
 
 
-#Knapp för att starta om spelet
-restart_button = Button(text="restart", font=("consoloas", 20), command=new_game)
-
 
 #En tom lista för knapparna att läggas in i en 5x5 storlek
 setup =         [[0,0,0,0,0],
@@ -300,13 +283,8 @@ gameplay_frame = Frame(window)
 
 setup_button = Button(text="Confirm", font=("consoloas", 20), command=lambda: confirm())
 
-player1_setup_button = Button(text="Confirm", font=("consoloas", 20), command=lambda :confirm(1)) 
-
-player2_setup_button = Button(text="Confirm", font=("consoloas", 20), command=lambda :confirm(2))
-
-
 #Loopar för att skapa knapparna till spelbrädet
-#En loop för varje bräde
+#En loop för vardera av bräden
 for row in range(5):
 
     for column in range(5):
@@ -320,11 +298,12 @@ for row in range(5):
 
     for column in range(5):
 
-        gameplay[row][column] = Button(gameplay_frame, text="", font=("consolas", 40), width=3, height=1, command= lambda row=row, column=column: hit_ships(row, column, you))
+        gameplay[row][column] = Button(gameplay_frame, text="", font=("consolas", 40), width=3, height=1, command= lambda row=row, column=column: hit_ships(row, column))
 
         gameplay[row][column].grid(row=row,column=column)
 
 window.mainloop()
 
+#Ser till att om rutan stängs och det finns en koppling så avslutas den
 if client != None:
     client.close()
